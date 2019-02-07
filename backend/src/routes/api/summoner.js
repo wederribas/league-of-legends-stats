e179dd1f4b69bd762c20804aa11f566e72b72531
Weder Ribas <wederribas@gmail.com>
2019-02-06 22:41:21 -0200

    Create routes to retrieve summoners data and their matches

--- backend/src/routes/api/summoner.js
+++ backend/src/routes/api/summoner.js
@@ -1,7 +1,36 @@
 const router = require('express').Router()
+const {
+  getSummonerByName,
+  getRecentMatchesByAccountId,
+  InvalidSummonerName
+} = require('../../lib/utils')
 
-router.get('/:summonerName', function(req, res, next) {
-  return res.json(`It worked, within /summoners/${req.params.summonerName}`)
+router.get('/:summonerName', async function(req, res, next) {
+  try {
+    const summoner = await getSummonerByName(req.params.summonerName)
+    return res.json(summoner)
+  } catch (err) {
+    if (err instanceof InvalidSummonerName) {
+      return res.status(422).json({ message: err.message })
+    }
+
+    return res.status(404).json({ message: 'Summoner name not found' })
+  }
+})
+
+router.get('/:accountId/matches', async function(req, res, next) {
+  try {
+    const summoner = await getRecentMatchesByAccountId(req.params.accountId)
+    return res.json(summoner)
+  } catch (err) {
+    if (err instanceof InvalidSummonerName) {
+      return res.status(422).json({ message: err.message })
+    }
+
+    return res
+      .status(404)
+      .json({ message: 'No matches found for the given account ID' })
+  }
 })
 
 module.exports = router
